457e12499693affa2fde99a3ee6de20f61c251a2
Sathish <sathishreddy00@gmail.com>
2020-05-27 17:59:23 +0530

    left hand segment done

--- src/App/Layout/App.js
+++ src/App/Layout/App.js
@@ -1,11 +1,18 @@
-import React,{Component} from 'react';
+import React,{Component,Fragment} from 'react';
+import {Container} from 'semantic-ui-react';
+
+import EventDashboard from '../../Features/Event/EventDashboard/EventDashboard';
+import NavBar from '../../Features/Nav/NavBar/NavBar';
 
 class App extends Component {
   render(){
     return (
-      <div>
-        <h1>Hi !! welcome!!! sathish!!!</h1>
-      </div>
+      <Fragment>
+        <NavBar/>
+        <Container className='main'>
+          <EventDashboard/>
+        </Container>
+      </Fragment>
     );
   }
 
---
+++ src/Features/Event/EventDashboard/EventDashboard.js
@@ -0,0 +1,21 @@
+import React , {Component} from 'react';
+import {Grid}  from 'semantic-ui-react';
+
+import EventList from '../EventList/EventList';
+
+class EventDashboard extends Component{
+    render(){
+        return(
+            <Grid>
+                <Grid.Column width={10}>
+                    <EventList/>
+                </Grid.Column>
+                <Grid.Column width={6}>
+                   <h1>Right Column</h1> 
+                </Grid.Column>
+            </Grid>
+        )
+    }
+}
+
+export default  EventDashboard;
